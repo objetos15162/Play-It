7e3f4913a11575287f8c983459e282cf290c5337
LiliaCH <lilia.el44@gmail.com>
2016-05-20 13:05:12 -0500

    cambio a la clase choose

--- Proyecto/MenuPiezas.java
+++ Proyecto/MenuPiezas.java
@@ -13,10 +13,12 @@ public class MenuPiezas extends World
      * Constructor para la clase menu_piezas.
      * 
      */
+    private GreenfootImage ima1;
     public MenuPiezas()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600,400,1);
+        ima1 = new GreenfootImage("choose.png");
         prepare();
     }
     public void act()
@@ -27,19 +29,17 @@ public class MenuPiezas extends World
     
     public void prepare()
     {
-
+        getBackground().drawImage(ima1,75,0);
         PDueto p_dueto = new PDueto();
         addObject(p_dueto,105,264);
         PTrio p_trio = new PTrio();
         addObject(p_trio,294,264);
         PCuarteto p_cuarteto = new PCuarteto();
         addObject(p_cuarteto,482,264);
-       
-        Choose choose = new Choose(); 
-        addObject(choose,298,71);
         
         BMainMenu main_menu = new BMainMenu();
         addObject(main_menu,27,384);
         
     }
+    
 }
