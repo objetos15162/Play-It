53c1265cb95df03fa0e6237c44f7f9aa925cf14f
LiliaCH <lilia.el44@gmail.com>
2016-05-20 13:26:04 -0500

    Correction de sintaxis y declaraciones

--- Proyecto/Ensamble.java
+++ Proyecto/Ensamble.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.ArrayList;
 /**
  * Clase escenarios que contiene los escenarios de los distintos duetos
  * 
@@ -11,37 +11,72 @@ public class Ensamble extends KinectWorld
 {
 
     /**
-     * Constructor for objects of class escenarios.
+     * Constructor for objects of class Ensamble
      * 
      */
-     BMainMenu main_menu3;
+     private BMainMenu main_menu3;
+     private Vida vidas1; 
+     private Vida vidas2;
+     private Vida vidas3;
+     private Vida vidas4;
+     private Vida vidas5;
+     private ArrayList<Vida> vidas;
     public Ensamble()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
        main_menu3 = new BMainMenu();
+       vidas1 = new Vida();
+       vidas2 = new Vida();
+       vidas3 = new Vida();
+       vidas4 = new Vida();
+       vidas5 = new Vida();
         prepare();
-      
+      fillArrayVidas();
     }
     private void prepare()
     {
-        Vida vidas = new Vida();
-        addObject(vidas,574,368);
-        vidas.setLocation(570,363);
-        Vida vidas2 = new Vida();
+        
+        addObject(vidas1,574,368);
+        vidas1.setLocation(570,363);
+        
         addObject(vidas2,548,368);
         vidas2.setLocation(545,364);
-        Vida vidas3 = new Vida();
+        
         addObject(vidas3,525,374);
         vidas3.setLocation(519,363);
         vidas3.setLocation(521,365);
-        Vida vidas4 = new Vida();
+       
         addObject(vidas4,500,370);
         vidas4.setLocation(497,365);
-        Vida vidas5 = new Vida();
+        
         addObject(vidas5,473,365);
         addObject(main_menu3,28,385);
     }
-    
+    /**
+     * método para rellenar el arreglo de vidas con las vidas
+     */
+    private void fillArrayVidas()
+    {   
+        vidas.add(vidas1);
+        vidas.add(vidas2);
+        vidas.add(vidas3);
+        vidas.add(vidas4);
+        vidas.add(vidas5);
+    }
+    /**
+     * método para regresar vidas
+     */
+    public ArrayList<Vida> getVidas()
+    {
+        return vidas;
+    }
+    /**
+     * método para remover una vida
+     */
+    public void removeVida()
+    {
+        vidas.remove(vidas.size()-1);
+    }
     
     
 }
