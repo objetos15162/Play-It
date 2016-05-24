666ff05d0b52855abd7168a72ad6e851b6bef1a9
EsauBz <produc.galofrantes@gmail.com>
2016-05-24 17:22:29 -0500

    Update Violinistab.java

--- Proyecto/Violinistab.java
+++ Proyecto/Violinistab.java
@@ -23,7 +23,7 @@ public class Violinistab extends Musico
         setImage(im1);
     }
       /*
-      *En este metodo se manda llamar al musicians de su super clase, que es donde surge el intercambio de 
+      *En este metodo se manda llamar al musiciansMove de su super clase, que es donde surge el intercambio de 
       *  imagenes para la animacion.
      */
     public void musiciansMove()
