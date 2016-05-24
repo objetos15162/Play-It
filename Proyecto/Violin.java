899c95778b37d35044367b784117e565e8368af5
EsauBz <produc.galofrantes@gmail.com>
2016-05-24 18:26:04 -0500

    Update Violin.java

--- Proyecto/Violin.java
+++ Proyecto/Violin.java
@@ -1,23 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Violin here.
- * 
+ * El detector encargado de la linea Violin,cada mundo si asi lo requiere llamara uno para asignarlo
+ * a la linea correspondiente, esta clase hereda de Detector. 
  * @author (Esaú & Lilia)
  * @version (may 2016)
  */
 public class Violin extends Detector
 {
     /**
-     * Act - do whatever the Violin wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Llama al act de la super clase, se encargara de saber si una clase instruccion esta tocando al objeto
      */
-   
     public void act() 
     {
-        // Add your act
         super.act();
-        //ion code here.
     }
     
 }
