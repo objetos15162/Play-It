9a0c7b40e7b7c9b2c52169d1bcbb4e235a07ab06
EsauBz <produc.galofrantes@gmail.com>
2016-05-24 20:16:05 -0500

    Update PDueto.java

--- Proyecto/PDueto.java
+++ Proyecto/PDueto.java
@@ -1,7 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Clase que da a escoger el menú para decidir qué ensamble se va a usar
+ * Esta clase representa a un boton que si es presionado nos generara una clase Transicion que le pasa a su constructor el numero 2
+ * indicando que nos llevara al mundo del Dueto.
  * 
  * @author (Lilia & Esaú) 
  * @version (may 2016)
@@ -9,7 +10,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class PDueto extends Boton
 {
     /**
-     * Act - llama a la trnasicion y le dice qué escenario cargar, en 
+     * Act - llama a la transicion y le dice qué escenario cargar, en 
      * este caso es el del dueto
      */
     public void act() 
