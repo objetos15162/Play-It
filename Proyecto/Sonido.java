10cbd484f6aa4daf5dc1549cfbfeea27d1238e8d
EsauBz <produc.galofrantes@gmail.com>
2016-05-24 19:37:32 -0500

    Update Sonido.java

--- Proyecto/Sonido.java
+++ Proyecto/Sonido.java
@@ -1,7 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Clase de sonido que contiene y controla los sonidos de las piezas
+ * Clase abstracta de la que heredan las clases, Dueto, Trio y Cuarteto, asi tenemos la obligacion de implementar los metodos 
+ * fillArrays y playSounds que deberan como su nombre lo dice, llenar un ArrayList con cada sonido que se reproducira en el nivel
+ * y tambien hacerlo sonar cuando asi le corresponda.
  * 
  * @author (Esaú & Lilia) 
  * @version (may 2016)
