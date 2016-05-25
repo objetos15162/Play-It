81721fafb43a576208198a904e625035d36a85be
EsauBz <produc.galofrantes@gmail.com>
2016-05-24 19:33:18 -0500

    Update Thumbnail.java

--- Proyecto/Thumbnail.java
+++ Proyecto/Thumbnail.java
@@ -1,7 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Thumbnail here.
+ * En esta clase se hace un acceso a la clase KinectWorld para obtener un "Thumbnail" una miniatura de la imagen que esta recibiendo
+ * el kinect en ese momento, al igualarla a una instancia GreenfootImage podemos ponerla dentro de cualquier mundo.
  * 
  * @author (EsauBz) 
  * @version (Mayo 2016)
@@ -12,8 +13,8 @@ public class Thumbnail  extends Actor
     {
     }
     /**
-     * Act - do whatever the Thumbnail wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act - Una instancia GreenfootImage para asi igualarla a la imagen que esta recibiendo el Kinect en el momento del act, esto
+     * mediante un acceso a la clase KinectWorld, que nos da la oportunidad de retornar una imagen.
      */
     public void act() 
     {
