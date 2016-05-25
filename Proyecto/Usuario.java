087bba8e6d426c146162a13bf6423d4b4c683ec4
EsauBz <produc.galofrantes@gmail.com>
2016-05-24 19:16:56 -0500

    Update Usuario.java

--- Proyecto/Usuario.java
+++ Proyecto/Usuario.java
@@ -1,6 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
- * Write a description of class Usuario here.
+ * Esta clase representa al jugador mientras se desarrolla el juego, tiene una instancia String donde se guarda el nombre
+ * y una instancia int para los puntos. Esta clase hereda de Actor.
  * 
  * @author (EsauBz) 
  * @version (Mayo 2016)
@@ -9,6 +10,9 @@ public class Usuario  extends Actor
 {
     private int puntos;
     private String name;
+    /**
+     * Constructor: Aqui se inicializa la variable name con caracteres vacios, y puntos de igual manera 0.
+     * */
     public Usuario()
     {
       name = "";
@@ -17,27 +21,34 @@ public class Usuario  extends Actor
       puntos = 0;
     }
     /**
-     * Act - do whatever the Usuario wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     *
+     * Metodo act de la clase Usuario
+     */
     public void act() 
     {
-        nombre="";
-        puntos=0;
-    }  */  
-
+    }   
+    /*
+    *@param String, variable que se recibe el cual sera el nombre que se le asignara al Usuario en su
+    variable name.
+    */
     public void setName(String nombre){
         name = nombre;
     }
-
+    /*
+    *@param int, variable que se recibe el cual sera la puntuacion que se le asignara al Usuario en su
+    variable puntos.
+    */
     public void setPoints(int p){
         puntos = p;
     }
-
+    /*
+    *@return String, retorna la variable name, que contiene el nombre del usuario.
+    */
     public String getName(){
         return name;
     }
-
+    /*
+    *@return int, retorna la variable puntos que contiene los puntos del usuario.
+    */
     public int getPoints(){
         return puntos;
     }    
